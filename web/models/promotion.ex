5edb7b7076058f11dc3bf1f8d3aa70dcc3916acc
Jdyn <moorejaden5389@gmail.com>
2019-09-04 18:07:29 -0700

    promotion init

---
+++ web/models/promotion.ex
@@ -0,0 +1,24 @@
+defmodule LolHero.Promotion do
+  use LolHero.Web, :model
+
+  alias LolHero.{Repo}
+
+  schema "promotions" do
+    field(:title, :string)
+    field(:description, :string)
+    field(:is_valid, :boolean, default: true)
+    field(:code, :string)
+
+    field(:use_count, :integer)
+
+    timestamps()
+  end
+
+  def changeset(promotion, attrs) do
+    promotion
+    |> cast(attrs, [:title, :description, :isValid, :code])
+    |> validate_required([:title, :description, :code])
+    |> unique_constraint(:title)
+    |> unique_constraint(:code)
+  end
+end
